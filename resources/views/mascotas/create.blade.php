790b60ef478367da6e08ffae509d0bcc621b3a69
Alumno <ulsa@iMacD105.local>
2019-03-11 18:10:52 -0700

    casilisto

--- resources/views/mascotas/create.blade.php
+++ resources/views/mascotas/create.blade.php
@@ -61,18 +61,22 @@
     function doChangePais(event) {
         $.get("/api/estados/" + $("#slcPais").val(),
         function (data) {
-            //console.log(data);
+            console.log(data);
             $("#slcEstado").empty();
-            $("#slcEstado").append(<option selected disabled value="">Elige un estado</option>);
+            $("#slcEstado").append('<option selected disabled value="">Elige un estado</option>');
             for(var i=0; i<data.length; i++){
-                
+                $("#slcEstado").append(
+                    '<option value="' +
+                    data[i].id + '">' + data[i].nombre +
+                    '</option>'
+                );
             }
         });
     }
 
 
     $(function () {
-        $("#slcPais").change();
+        $("#slcPais").change(doChangePais);
     });
 </script>
 
